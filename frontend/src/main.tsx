02239520c07216d348d0ecd571f243a5642193a0
Aarushi Hans <aarushihans.personal@gmail.com>
2026-07-16 14:19:30 +0530

    ui changes to signup page, created a new signup page and worked on that

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
-import { ThemeProvider } from "@/components/theme-provider"
+import { ThemeProvider } from "@/components/theme-provider";
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-		<BrowserRouter>
-			<ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-				<App />
-			</ThemeProvider>
-		</BrowserRouter>
+	<BrowserRouter>
+		<ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+			<App />
+		</ThemeProvider>
+	</BrowserRouter>
+
 );
